aea977ea519f1cf995186926134b0962575971e0
Chris <san.kyu@icloud.com>
2024-10-02 16:20:58 -0600

    First commit

---
+++ FrierenApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FrierenApp
+//
+//  Created by chris on 2024/10/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FrierenApp/FrierenAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrierenAppApp.swift
+//  FrierenApp
+//
+//  Created by chris on 2024/10/02.
+//
+
+import SwiftUI
+
+@main
+struct FrierenAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
